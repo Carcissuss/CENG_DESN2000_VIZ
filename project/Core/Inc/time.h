100407920359528d214fd62370848a157b60c4dd
iGriffin-1802 <vageesh.jha64@gmail.com>
2025-08-13 20:27:41 +1000

    draft alarm

--- project/Core/Inc/time.h
+++ project/Core/Inc/time.h
@@ -1,7 +1,10 @@
-
+#include "main.h"
 void timePage();
 void alarmPage();
 void switchTimeFormat();
 void switchAMPM();
 void changeAlarmHour();
 void changeAlarmMin();
+void alarmConfirm();
+void updateTime(uint8_t row, uint8_t col);
+void updateAlarm(uint8_t row, uint8_t col);
