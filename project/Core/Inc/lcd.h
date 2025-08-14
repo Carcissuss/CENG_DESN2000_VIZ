5f59606454186857aaaf7a49c76d784f3a43732d
George <113016308+Carcissuss@users.noreply.github.com>
2025-08-14 23:43:24 +1000

    Update lcd.h
    delete one unused function

--- project/Core/Inc/lcd.h
+++ project/Core/Inc/lcd.h
@@ -7,12 +7,11 @@ void LCD_PutNibble(uint8_t nibble);
 void LCD_SendCmd(uint8_t c);
 void LCD_SendData(uint8_t c);
 void LCD_SendStr(char *str);
-void LCD_SendTime(const char *str);
-
 
 //commands
 #define LCD_CLEAR_DISPLAY 0b00000001
 //... LCD_RETURN_HOME, LCD_SECOND_LINE....
 #define LCD_RETURN_HOME  0b00000010
 #define LCD_SECOND_LINE  0b11000000
+
 
